a85b3a9006416a123f22ed6d66fbc44c7f77ddb7
Danny Hsu <danny539425@gmail.com>
2024-01-07 17:41:36 +0800

    init

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    @IBOutlet weak var diceImage3: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImage1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImage2.image = #imageLiteral(resourceName: "DiceSix")
+        diceImage3.image = #imageLiteral(resourceName: "DiceSix")
     }
 
-
+    @IBAction func buttonPress(_ sender: Any) {
+//        print("button has been press\n")
+        diceImage1.image = #imageLiteral(resourceName: "DiceOne")
+        diceImage2.image = #imageLiteral(resourceName: "DiceOne")
+    }
+    
 }
 
