752640169ae35ad2934a4b922647b0a11820138e
Danny Hsu <danny539425@gmail.com>
2024-01-20 14:33:56 +0800

    0120

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,18 +12,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImage1: UIImageView!
     @IBOutlet weak var diceImage2: UIImageView!
     @IBOutlet weak var diceImage3: UIImageView!
-
+    var leftDiceNumber = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceImage1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImage1.image = #imageLiteral(resourceName: "DiceOne")
         diceImage2.image = #imageLiteral(resourceName: "DiceSix")
         diceImage3.image = #imageLiteral(resourceName: "DiceSix")
     }
 
     @IBAction func buttonPress(_ sender: Any) {
-//        print("button has been press\n")
-        diceImage1.image = #imageLiteral(resourceName: "DiceOne")
-        diceImage2.image = #imageLiteral(resourceName: "DiceOne")
+        
+        diceImage1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        diceImage2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        diceImage3.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
     }
     
 }
